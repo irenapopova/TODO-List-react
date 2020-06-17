25753a26d9b525c6450a484ef26659c49afe1eaa
Irena <irenejpopova6@gmail.com>
2020-06-17 08:19:30 +0200

    Updates

---
+++ precache-manifest.0db2294cabe560a4b5b2f126a307325e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd534858aef8b752418c93ddc7c0a2f",
+    "url": "/irenapopova/TODO-List-react/index.html"
+  },
+  {
+    "revision": "ffb650ec6717c7308e30",
+    "url": "/irenapopova/TODO-List-react/static/css/main.c76c5e98.chunk.css"
+  },
+  {
+    "revision": "7e54ad7dc85e3b38e4e1",
+    "url": "/irenapopova/TODO-List-react/static/js/2.66497c1e.chunk.js"
+  },
+  {
+    "revision": "ffb650ec6717c7308e30",
+    "url": "/irenapopova/TODO-List-react/static/js/main.5c8f7b3b.chunk.js"
+  },
+  {
+    "revision": "8e02ea430299125c2e37",
+    "url": "/irenapopova/TODO-List-react/static/js/runtime~main.7033b4b8.js"
+  },
+  {
+    "revision": "4f96b4e8e1bc3ca37ea4d86e1bd01960",
+    "url": "/irenapopova/TODO-List-react/static/media/bg.4f96b4e8.svg"
+  }
+]);
